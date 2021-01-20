d5d8ca13792cd2c2f2c7efa733b386b035c0a71f
Benjamin Earley <thebigben92@gmail.com>
2021-01-20 14:40:57 -0500

    dep updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-js"))
 
-    implementation("org.jetbrains:kotlin-react:17.0.1-pre.137-kotlin-1.4.10")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.137-kotlin-1.4.10")
-    implementation("org.jetbrains:kotlin-react-router-dom:5.2.0-pre.137-kotlin-1.4.10")
+    implementation("org.jetbrains:kotlin-react:17.0.1-pre.141-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.141-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-react-router-dom:5.2.0-pre.141-kotlin-1.4.21")
     implementation(npm("react", "17.0.1"))
     implementation(npm("react-dom", "17.0.1"))
     implementation(npm("react-router-dom", "5.2.0"))
 
-    implementation("org.jetbrains:kotlin-styled:5.2.0-pre.137-kotlin-1.4.10")
+    implementation("org.jetbrains:kotlin-styled:5.2.0-pre.141-kotlin-1.4.21")
     implementation(npm("styled-components", "~5.2.0"))
     implementation(npm("inline-style-prefixer", "~6.0.0"))
 
